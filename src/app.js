c92f63fc5ce2bd9acf2e020c7646ad6e1f821155
Diego <diego-jofre@live.com.ar>
2023-06-09 11:19:18 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
+import viewsRouter from "./routes/views.router.js";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", viewsRouter);
+
+const httpServer = app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+const io = new Server(httpServer);
+
+const messages = [];
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  io.emit("messageLogs", messages);
+
+  socket.on("message", (data) => {
+    messages.push(data);
+    io.emit("messageLogs", messages);
+  });
+});
